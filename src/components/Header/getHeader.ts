20c1f638fb5dde3619c715d4f8daf76477d8d183
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-03 19:36:10 +0200

    Add nav-bar to Header component

--- src/components/Header/getHeader.ts
+++ src/components/Header/getHeader.ts
@@ -1,13 +1,52 @@
+import setCurrentPageIndicator from "../utils/setCurrentPageIndicator";
 import "./Header.css";
 
-const getHeader = (): HTMLElement => {
+const getHeader = (handleShowSidebar: () => void): HTMLElement => {
   const header = document.createElement("header");
   header.className = "main-header";
 
-  header.innerHTML = `
-    <img class="page-logo" src="monument.svg" alt="Website logo" width="60" height="60"/>
-    <h1 class="page-title">monuments.</h1>
-    `;
+  const titleContainer = document.createElement("div");
+  titleContainer.className = "title-container";
+
+  const logo = document.createElement("img");
+  logo.className = "page-logo";
+  logo.src = "monument.svg";
+  logo.alt = "Website logo";
+  logo.width = 60;
+  logo.height = 60;
+
+  const longTitle = document.createElement("h1");
+  longTitle.classList.add("page-title", "page-title--long");
+  longTitle.textContent = "monuments.";
+
+  const shortTitle = document.createElement("h1");
+  shortTitle.classList.add("page-title", "page-title--short");
+  shortTitle.textContent = "mnmnts.";
+  shortTitle.ariaLabel = "monuments.";
+
+  titleContainer.append(logo, longTitle, shortTitle);
+
+  const headerNav = document.createElement("nav");
+  headerNav.className = "main-header__nav";
+
+  const navList = document.createElement("ul");
+  navList.className = "nav-list";
+  navList.innerHTML = "<li><a class='nav-list__item' href='/'>Home</a></li>";
+
+  setCurrentPageIndicator(navList);
+
+  headerNav.appendChild(navList);
+
+  const hamburgerMenuButton = document.createElement("button");
+
+  hamburgerMenuButton.className = "hamburger-menu";
+  hamburgerMenuButton.innerHTML = "&#9776;";
+
+  hamburgerMenuButton.addEventListener("click", () => {
+    handleShowSidebar();
+  });
+
+  header.append(titleContainer, headerNav, hamburgerMenuButton);
 
   return header;
 };
